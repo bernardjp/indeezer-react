135653d45913f5d48c5d46835dde33c90afe950a
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-03-16 10:52:08 -0300

    updated React-Query provider and Mantine provider as HoC

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import Header from './components/Header/Header';
 import AboutPage from './components/About.page';
 import HomePage from './components/Home.page';
 import ExplorePage from './components/Explore.page';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="explore/*" element={<ExplorePage />} />
         <Route path="about" element={<AboutPage />} />
       </Routes>
-    </QueryClientProvider>
+    </>
   );
 }
 
