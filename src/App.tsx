331effefb51edccf5a09c2e9a631eb5b37d718b6
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-04-06 19:04:26 -0300

    updated routing: nested the explore routes inside the explore.page component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import AboutPage from './components/About.page';
 import HomePage from './components/Home.page';
 import ExplorePage from './components/Explore.page';
-import MainDisplay from './components/ResourceDisplay/MainDisplay';
 
 function App() {
   return (
@@ -11,13 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="explore" element={<ExplorePage />}>
-          <Route path="artists" element={<MainDisplay />} />
-          <Route path="albums" element={<MainDisplay />} />
-          <Route path="tracks" element={<MainDisplay />} />
-          <Route path="playlists" element={<MainDisplay />} />
-          <Route path="podcasts" element={<MainDisplay />} />
-        </Route>
+        <Route path="explore/*" element={<ExplorePage />} />
         <Route path="about" element={<AboutPage />} />
       </Routes>
     </>
