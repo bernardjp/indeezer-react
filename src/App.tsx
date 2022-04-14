7ed6d9871b8854c894490eda6cfa0fa7c885b070
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-04-14 19:23:44 -0300

    added wrapper container to the app layout, and added placeholder footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
+import GeneralLayoutContainer from './components/GeneralLayoutContainer';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import AboutPage from './components/About.page';
 import HomePage from './components/Home.page';
 import ExplorePage from './components/Explore.page';
 
 function App() {
   return (
-    <>
+    <GeneralLayoutContainer>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="explore/*" element={<ExplorePage />} />
         <Route path="about" element={<AboutPage />} />
       </Routes>
-    </>
+      <Footer />
+    </GeneralLayoutContainer>
   );
 }
 
