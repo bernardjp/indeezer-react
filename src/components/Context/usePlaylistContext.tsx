af7673dc44cf1cbbd6ce6708aa71ef573dfdcb11
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-12-16 18:26:33 -0300

    changed the playlist data-structure from Set to Array. Added helper functions to handle changes on the playlist

--- src/components/Context/usePlaylistContext.tsx
+++ src/components/Context/usePlaylistContext.tsx
@@ -1,23 +1,56 @@
 import { useState } from 'react';
+import { TrackType } from '../../types/AudioPlayer.types';
+
+// HELPER FUNCTIONS ---
+const checkTrackDuplicates = (list: Array<TrackType>, newTrack: TrackType): boolean => {
+  const duplicates = list.filter((track: TrackType) => track.trackID === newTrack.trackID);
+  return Boolean(duplicates.length);
+};
+
+const filterTrackById = (list: Array<TrackType>, id: number): Array<TrackType> => {
+  const filteredTracks = list.filter((track: TrackType) => track.trackID !== id);
+  return filteredTracks;
+};
+
+// Richard Durstenfeld modern version of the Fisher-Yates algorithm to
+// shuffle arrays designed for computer use.
+// TO-DO: Move to UTILS folder
+// eslint-disable-next-line no-unused-vars
+export const shuffleArray = (list: Array<number>) => {
+  const newList = [...list];
+
+  for (let i = newList.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = newList[i];
+    newList[i] = newList[j];
+    newList[j] = temp;
+  }
+
+  return newList;
+};
 
 function usePlaylistContext() {
-  const [playlist, setPlaylist] = useState<Set<string>>(new Set());
+  const [playlist, setPlaylist] = useState<Array<TrackType>>([]);
 
-  const addTrack = (track: string): void => {
-    const newPlaylist = new Set(playlist).add(track);
+  // const getTrack = (index: number): TrackType => playlist[index];
+
+  const addTrack = (track: TrackType): void => {
+    const isDuplicated = checkTrackDuplicates(playlist, track);
+    if (!isDuplicated) setPlaylist([...playlist, track]);
+  };
+
+  const removeTrack = (id: number): void => {
+    const newPlaylist = filterTrackById(playlist, id);
     setPlaylist(newPlaylist);
   };
 
-  const removeTrack = (track: string): void => {
-    const newPlaylist = new Set(playlist);
-    newPlaylist.delete(track);
-
-    setPlaylist(newPlaylist);
+  const clearList = (): void => {
+    setPlaylist([]);
   };
 
   return {
-    playlist, setPlaylist, addTrack, removeTrack
+    playlist, addTrack, removeTrack, clearList
   };
 }
 
-export default usePlaylistContext;
+export { usePlaylistContext };
