035df839136850c0f708455e7b9b25ce1f8248a2
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-12-21 19:40:15 -0300

    added AudioPlayer base button Component. Early implementation

---
+++ src/components/AudioPlayer/AudioPlayerButton.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+import { Button, Tooltip } from '@mantine/core';
+
+type Props = {
+  tooltip: string,
+  type: [
+    'play', 'pause', 'next', 'prev', 'share', 'loop_list', 'loop_track', 'shuffle', 'volume', 'eq', 'lyrics', 'like'
+  ],
+  isDisable: boolean,
+  isActive: boolean,
+  size: ['sm', 'm', 'lg'],
+  onClickHandler: () => {}
+}
+
+/*
+  BUTTON Props:
+    - Object-like props: {
+      tootip: string || ""
+      type: [
+        "play", "pause", "next", "prev", "share", "loop_list",
+        "loop_track", "shuffle", "volume", "eq", "lyrics", "like"
+      ], --------> set the button icon
+      isDisable: boolean,
+      isActive: boolean, ---> icon stays colored when active (optional arguement)
+      size: ["sm", "m", "lg"],
+      onClickHandler: () => {}
+    }
+
+  icon: {
+    play: faplayicon ---> icon from react-icons library
+  }
+  --> icon[type]
+*/
+
+function AudioPlayerButton(props: Props): JSX.Element {
+  const {
+    tooltip,
+    type,
+    isDisable,
+    isActive,
+    size,
+    onClickHandler
+  } = props;
+
+  return (
+    <Tooltip label={tooltip}>
+      <Button onClick={onClickHandler}>
+        {type}
+      </Button>
+    </Tooltip>
+  );
+}
+
+export default AudioPlayerButton;
