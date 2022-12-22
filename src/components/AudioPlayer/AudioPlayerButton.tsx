028b1b7fd8eacd1f8dafe80dd40011b4abfdf9a8
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-12-22 20:30:14 -0300

    added styles to the audioplayer track controls

--- src/components/AudioPlayer/AudioPlayerButton.tsx
+++ src/components/AudioPlayer/AudioPlayerButton.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-vars */
-import { Button, Tooltip } from '@mantine/core';
-import { BsFillPlayFill, BsPause } from 'react-icons/bs';
-import { IoPlaySkipBackSharp, IoPlaySkipForwardSharp } from 'react-icons/io5';
+import { Button, Tooltip, createStyles } from '@mantine/core';
+import {
+  IoPlaySkipBackSharp, IoPlaySkipForwardSharp, IoPlaySharp, IoPauseSharp
+} from 'react-icons/io5';
 
 type Props = {
   tooltip: string,
@@ -14,8 +16,8 @@ type Props = {
 }
 
 const icons = {
-  play: <BsFillPlayFill />,
-  pause: <BsPause />,
+  play: <IoPlaySharp style={{ paddingLeft: '3px' }} />,
+  pause: <IoPauseSharp />,
   next: <IoPlaySkipForwardSharp />,
   prev: <IoPlaySkipBackSharp />
   // share: '',
@@ -27,6 +29,50 @@ const icons = {
   // lyrics: '',
   // like: ''
 };
+
+// const sizeParams = {
+//   lg: {
+//     fontSize: '1.8rem',
+//     height: '48px',
+//     width: '48px'
+//   },
+//   m: {
+//     fontSize: '1.4rem',
+//     height: '32px',
+//     width: '32px'
+//   },
+//   sm: {
+//     fontSize: '1rem',
+//     height: '24px',
+//     width: '24px'
+//   }
+// };
+
+const useStyles = createStyles((theme, params: Props) => ({
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderRadius: '50%',
+    color: 'white',
+    display: 'flex',
+    fontSize: params.size === 'lg' ? '1.8rem' : params.size === 'm' ? '1.2rem' : '1rem',
+    height: params.size === 'lg' ? '48px' : params.size === 'm' ? '36px' : '24px',
+    justifyContent: 'center',
+    padding: '0',
+    textAlign: 'center',
+    width: params.size === 'lg' ? '48px' : params.size === 'm' ? '32px' : '24px',
+    transition: '0.25s',
+
+    '&:hover': {
+      backgroundColor: '#42424c'
+    },
+
+    '&:disabled': {
+      backgroundColor: 'transparent',
+      color: '#3e3e47'
+    }
+  }
+}));
 
 /*
   BUTTON Props:
@@ -58,9 +104,12 @@ function AudioPlayerButton(props: Props): JSX.Element {
     onClickHandler
   } = props;
 
+  const { classes } = useStyles(props);
+
   return (
     <Tooltip label={tooltip} disabled={tooltip === ''}>
       <Button
+        className={classes.button}
         onClick={onClickHandler}
         disabled={isDisable}
       >
