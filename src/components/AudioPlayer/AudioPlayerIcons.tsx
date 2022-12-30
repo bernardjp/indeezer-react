45a3b8903b18d98013a089e1fb91c81c87915709
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-12-30 12:40:00 -0300

    added 'like', 'add', and 'lyrics' icons

--- src/components/AudioPlayer/AudioPlayerIcons.tsx
+++ src/components/AudioPlayer/AudioPlayerIcons.tsx
@@ -1,10 +1,11 @@
 import {
-  IoPlaySkipBackSharp, IoPlaySkipForwardSharp, IoPlaySharp, IoPauseSharp
+  IoPlaySkipBackSharp, IoPlaySkipForwardSharp, IoPlaySharp, IoPauseSharp, IoHeartOutline
 } from 'react-icons/io5';
 import { TiArrowLoop, TiArrowShuffle } from 'react-icons/ti';
 import { FaChromecast } from 'react-icons/fa';
-import { FiVolume2, FiVolumeX } from 'react-icons/fi';
+import { FiVolume2, FiVolumeX, FiPlus } from 'react-icons/fi';
 import { ImEqualizer } from 'react-icons/im';
+import { BiMicrophone } from 'react-icons/bi';
 
 const icons = {
   play: <IoPlaySharp style={{ paddingLeft: '3px' }} />,
@@ -12,14 +13,14 @@ const icons = {
   next: <IoPlaySkipForwardSharp />,
   prev: <IoPlaySkipBackSharp />,
   loop_list: <TiArrowLoop style={{ fontSize: '1.4rem' }} />,
-  // loop_track: '',
   share: <FaChromecast style={{ fontSize: '1.2rem' }} />,
   shuffle: <TiArrowShuffle style={{ fontSize: '1.3rem' }} />,
   volume_on: <FiVolume2 style={{ fontSize: '1.1rem' }} />,
   volume_off: <FiVolumeX style={{ fontSize: '1.1rem' }} />,
-  eq: <ImEqualizer style={{ fontSize: '1rem' }} />
-  // lyrics: '',
-  // like: ''
+  eq: <ImEqualizer style={{ fontSize: '1rem' }} />,
+  lyrics: <BiMicrophone style={{ fontSize: '1.2rem' }} />,
+  like: <IoHeartOutline style={{ fontSize: '1.2rem' }} />,
+  add: <FiPlus style={{ fontSize: '1.2rem' }} />
 };
 
 export default icons;
