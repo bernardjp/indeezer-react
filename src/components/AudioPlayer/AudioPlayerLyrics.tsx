1d786c113cd1bcb54441b9e5b1bd82c53d768774
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-01-06 21:57:49 -0300

    added Lyrics Overlay support

---
+++ src/components/AudioPlayer/AudioPlayerLyrics.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import {
+  Drawer, Image, Text, createStyles
+} from '@mantine/core';
+import { AudioPlayerButton } from './AudioPlayerButton';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+  },
+  drawer: {
+    padding: '2.5rem !important',
+    background: `linear-gradient(233.13deg, rgb(246, 105, 60) 0%, ${theme.colors.red[6]} 89.29%)`
+  },
+  header: {
+    justifyContent: 'flex-start',
+    margin: '0'
+  },
+  closeButton: {
+    height: '40px',
+    width: '40px',
+    borderRadius: '50%',
+
+    '&:hover': {
+      backgroundColor: '#ffffff2e'
+    },
+
+    '& svg': {
+      height: '32px',
+      width: '32px'
+    }
+  },
+  body: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 'calc(100% - 80px)',
+    padding: '0 4rem 80px 4rem'
+  },
+  cover: {
+    filter: 'drop-shadow(rgba(0, 0, 0, 0.1) 0px 5.2392px 20.9569px) drop-shadow(rgba(0, 0, 0, 0.1) 0px 20.9569px 41.9137px)'
+  },
+  lyrics: {
+    width: '500px',
+    textAlign: 'center',
+    opacity: '0.65',
+    transition: '0.1s',
+
+    '&:hover': {
+      cursor: 'pointer',
+      opacity: '1'
+    }
+  }
+}));
+
+function AudioPlayerLyricsOverlay(props: { image: string }): JSX.Element {
+  const { image } = props;
+
+  const [opened, setOpened] = useState(false);
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <Drawer
+        classNames={{
+          root: classes.root,
+          drawer: classes.drawer,
+          header: classes.header,
+          closeButton: classes.closeButton,
+          body: classes.body
+        }}
+        position="bottom"
+        opened={opened}
+        onClose={() => setOpened(false)}
+        transitionDuration={500}
+        size="full"
+        zIndex={3}
+      >
+        <Image
+          className={classes.cover}
+          src={image}
+          width={320}
+          radius={16}
+        />
+        <Text
+          className={classes.lyrics}
+          size={42}
+          fw="bolder"
+        >Lyrics
+        </Text>
+      </Drawer>
+      <AudioPlayerButton
+        tooltip="View lyrics"
+        size="m"
+        type="lyrics"
+        onClickHandler={() => setOpened((val) => !val)}
+        isDisable={false}
+        isActive={false}
+      />
+    </>
+  );
+}
+
+export default AudioPlayerLyricsOverlay;
