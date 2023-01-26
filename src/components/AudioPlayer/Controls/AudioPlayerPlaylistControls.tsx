76d86097c1f0f2d41788efec7acbd46cf81c35c9
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-01-25 21:09:10 -0300

    added mobile adjustments to the track control button of the Audioplayer component

--- src/components/AudioPlayer/Controls/AudioPlayerPlaylistControls.tsx
+++ src/components/AudioPlayer/Controls/AudioPlayerPlaylistControls.tsx
@@ -4,16 +4,32 @@ import { createStyles } from '@mantine/core';
 import AudioPlayerPlaylistOverlay from '../Playlist/PlaylistOverlay';
 import { AudioPlayerButton, AudioPlayerVolumeButton, AudioPlayerEQButton } from '../AudioPlayerButton';
 import useAudioPlayerStore from '../../../store/AudioPlayerStore';
+import useTracksStore from '../../../store/TracksStore';
 
-const useStyles = createStyles(() => ({
+const useStyles = createStyles((theme, params: { current: boolean }) => ({
   mainContainer: {
     alignItems: 'center',
-    display: 'flex'
+    display: 'flex',
+
+    [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
+      display: params.current ? 'flex' : 'none',
+      position: 'absolute',
+      bottom: '8px',
+      right: '16px'
+    }
   },
   controlContainer: {
     alignItems: 'center',
     display: 'flex',
-    gap: '8px'
+    gap: '8px',
+
+    [`@media (max-width: ${theme.breakpoints.lg}px)`]: {
+      gap: '4px'
+    },
+
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      display: 'none'
+    }
   }
 }));
 
@@ -24,8 +40,9 @@ function AudioPlayerPlaylistControls(): JSX.Element {
     toggleLooping,
     toggleShuffle
   } = useAudioPlayerStore((state) => state);
+  const { current } = useTracksStore();
 
-  const { classes } = useStyles();
+  const { classes } = useStyles({ current: Boolean(current) });
 
   return (
     <div className={classes.mainContainer}>
