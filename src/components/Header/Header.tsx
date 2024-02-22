24ca2b177178629d2a7dc9d50ea484a65260dc23
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-02-22 15:51:17 -0300

    Fixed a bug that prevented the header and footer to change color when switching themes.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,13 @@ import BurgerMenu from './BurgerMenu/BurgerMenu';
 const useStyles = createStyles((theme) => ({
   header: {
     alignItems: 'center',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
-    borderBottom: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[7]
+        : theme.colors.gray[0],
+    borderBottom: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
     boxShadow: '0px 1px 20px -10px rgba(0,0,0,0.3)',
     display: 'flex',
     height: '54px',
@@ -21,9 +26,9 @@ const useStyles = createStyles((theme) => ({
 
     [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
       backdropFilter: 'blur(16px)',
-      backgroundColor: '#121216d1'
-    }
-  }
+      // backgroundColor: '#121216d1',
+    },
+  },
 }));
 
 function StyledHeader(props: { withSearchBar: boolean }) {
