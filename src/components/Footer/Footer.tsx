f27f7ab8494b27dd2be7c9f5c6917c68d2faf4e1
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-02-22 15:51:17 -0300

    Fixed a bug that prevented the header and footer to change color when switching themes.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,10 +5,18 @@ import { AudioPlayerContextProvider } from '../Context/AudioPlayerContext';
 const useStyles = createStyles((theme) => ({
   footerContainer: {
     alignItems: 'center',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-    borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]}`,
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[5]
+        : theme.colors.gray[0],
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]
+    }`,
     bottom: 0,
-    boxShadow: theme.colorScheme === 'dark' ? '0px -1px 20px -8px rgba(0,0,0,0.5)' : '0px 0px 30px -8px rgba(0,0,0,0.1)',
+    boxShadow:
+      theme.colorScheme === 'dark'
+        ? '0px -1px 20px -8px rgba(0,0,0,0.5)'
+        : '0px 0px 30px -8px rgba(0,0,0,0.1)',
     display: 'flex',
     height: '80px',
     justifyContent: 'center',
@@ -22,9 +30,12 @@ const useStyles = createStyles((theme) => ({
       height: 'fit-content',
       minHeight: '146px',
       backdropFilter: 'blur(16px)',
-      backgroundColor: '#23232dde'
-    }
-  }
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[5]
+          : theme.colors.gray[0],
+    },
+  },
 }));
 
 function Footer() {
