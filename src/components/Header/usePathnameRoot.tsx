c2dcaaded3f36621ad783aec67fc09e96b0db070
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-02-23 14:46:11 -0300

    Adjusted the usePathnameRoot custom hook to take account of the new pathname.

--- src/components/Header/usePathnameRoot.tsx
+++ src/components/Header/usePathnameRoot.tsx
@@ -2,7 +2,7 @@ import { useLocation } from 'react-router';
 
 function usePathnameRoot(): string {
   const { pathname } = useLocation();
-  const pathnameRoot = `/${pathname.split('/')[1]}`;
+  const pathnameRoot = `${pathname.split('/')[2] || '/indeezer-react/'}`;
 
   return pathnameRoot;
 }
